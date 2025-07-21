a4b100325822953990caa6e38b33921ceb6993f9
Gahan <z5477979@ad.unsw.edu.au>
2025-07-21 18:03:40 +1000

    Script creates csvs containing mvp voting data from the 1980 to 2025 season.

--- nba-mvp/data/mvp-voting/voting-scraper.py
+++ nba-mvp/data/mvp-voting/voting-scraper.py
@@ -1,22 +1,48 @@
-# %%
-
-# Imports
 from bs4 import BeautifulSoup
 import pandas as pd
 import requests
-from selenium import webdriver
-import chromedriver_binary
+import time
 
-# %%
-# Algorithm
+# Setting up the driver for the algorithm
+year = 2025
+while year <= 2025:
+    # Getting the html for the required MVP voting pages from Basketball Reference
+    # to convert to soup
+    url = f"https://www.basketball-reference.com/awards/awards_{year}.html"
+    page = requests.get(url, headers={'User-agent': 'your bot 0.1'})
+    soup = BeautifulSoup(page.text, "html.parser")
 
-# Getting the html for the required MVP voting pages from Basketball Reference and converting it into soup
-year = 1980
-url = f"https://www.basketball-reference.com/awards/awards_{year}.html"
-driver = webdriver.Chrome()
-driver.get(url)
-driver.implicitly_wait(100)
-page = driver.page_source()
-soup = BeautifulSoup(page.text, "html.parser")
+    # Finding the required mvp voting table
+    table = soup.find_all("table", limit=1)[0]
 
-# %%
+    # Extracting the headers from the table
+    headers = table.find_all("th")
+    headers = [header.text for header in headers]
+
+    # Finding the headers needed
+    s = headers.index("Player")
+    e = headers.index("Share")
+    headers = headers[s:e+1]
+
+    # creating the data frame to store the mvp voting data
+    df = pd.DataFrame(columns=headers)
+
+    # Extracting rws from the table
+    rows = table.find_all("tr")
+
+    # Extracting cell data from each row and adding to the dataframe
+    dfsize = 0
+    for row in rows:
+
+        rowdata = []
+        for cell in row:
+            rowdata.append(cell.text.strip())
+
+        if rowdata[0] != "":
+            df.loc[dfsize] = rowdata[1:8]
+            dfsize += 1
+
+    # Exporting to a csv for use in machine learning
+    df.to_csv(f"{year-1}-{year}-mvp-voting.csv")
+    year += 1
+    time.sleep(5)
