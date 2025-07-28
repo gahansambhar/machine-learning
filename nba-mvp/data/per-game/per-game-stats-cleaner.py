297480080c517da797f4bc23fa2e6e1d4d19bb2d
Gahan <z5477979@ad.unsw.edu.au>
2025-07-28 14:09:30 +1000

    Removed data that contained @TM or 3TM etc from advanced and per game stats

---
+++ nba-mvp/data/per-game/per-game-stats-cleaner.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+year = 1980
+
+while year <= 2025:
+    file = f"{year-1}-{year}-per-game-stats.csv"
+    df = pd.read_csv(file)
+
+    df = df[~df['Team'].isin(["2TM", "3TM", "4TM", "5TM"])]
+    df.to_csv(file, index=False)
+    year += 1
