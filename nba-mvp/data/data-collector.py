880c636651b7d18b139b0e509780201ac38e47b9
Gahan <gsambhar@gmail.com>
2025-08-24 00:11:50 +1000

    final data file has been made

--- nba-mvp/data/data-collector.py
+++ nba-mvp/data/data-collector.py
@@ -1,7 +1,9 @@
 import pandas as pd
 
-year = 2025
+year = 1980
 
+output = pd.DataFrame()
+# Loop through each year from 1980 to 2025
 while year <= 2025:
     advanced = pd.read_csv(f"advanced/{year-1}-{year}-advanced-stats.csv")
     pergame = pd.read_csv(f"per-game/{year-1}-{year}-per-game-stats.csv")
@@ -9,7 +11,15 @@ while year <= 2025:
     standings = pd.read_csv(
         f"team-standings/{year-1}-{year}-team-standings.csv")
 
-    output = pd.merge(advanced, pergame, on=[
+    curr = pd.merge(pergame, advanced, on=[
                       "Player", "Age", "Team", "Pos", "G", "GS"], )
+    
+    curr = pd.merge(curr, standings, on=["Team"]) 
+    curr = pd.merge(curr, voting, on=["Player", "Team"], how="outer")
+    curr["Share"].fillna(0, inplace=True)
+    curr.insert(0, "Season", year)
+    output = pd.concat([output, curr], ignore_index=True)
 
     year += 1
+
+output.to_csv("data.csv", index=False)
