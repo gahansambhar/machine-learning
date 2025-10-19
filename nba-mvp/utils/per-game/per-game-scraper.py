d59cbe57bdb5a5de58b067509a094b3bad42b5e1
Gahan Sambhar <gsambhar@gmail.com>
2025-10-19 18:38:49 +1100

    Major Porject structure change in favour of replicability.

---
+++ nba-mvp/utils/per-game/per-game-scraper.py
@@ -0,0 +1,47 @@
+from bs4 import BeautifulSoup
+import pandas as pd
+import requests
+import time
+
+
+def fetch_per_game():
+    year = 1980
+
+    while year <= 2025:
+
+        # Extracting HTML content from required basketball reference page
+        url = f"https://www.basketball-reference.com/leagues/NBA_{year}_per_game.html"
+        page = requests.get(url)
+        soup = BeautifulSoup(page.text, "html.parser")
+
+        # Finding the player stats table
+        table = soup.find_all("table")[0]
+
+        # Grabbing the headers for the table
+        headers = table.find_all("th")
+        headers = [header.text for header in headers]
+
+        # Creating the dataframe to store the data
+        df = pd.DataFrame(columns=headers)
+
+        # Processing each row individually and adding them to the dataframe
+        rows = table.find_all("tr")
+        dfsize = 0
+
+        for row in rows:
+            rowdata = []
+
+            for cell in row:
+                rowdata.append(cell.text.strip())
+
+            rowdata = [rowdata[i] for i in range(len(rowdata)) if i % 2 == 1]
+
+            if rowdata[0] != "Rk" and rowdata[0] != "":
+                df.loc[dfsize] = rowdata
+                dfsize += 1
+
+        # Extracting info to a csv
+        df.to_csv(f"{year-1}-{year}-per-game-stats.csv", index=False)
+
+        year += 1
+        time.sleep(5)
